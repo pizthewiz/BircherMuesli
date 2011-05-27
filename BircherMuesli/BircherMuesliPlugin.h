b41a809d66528481f37919d6edb2a7de4f1d38bd
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-05-26 20:09:34 -0700

    sanitize patch template code

--- BircherMuesli/BircherMuesliPlugin.h
+++ BircherMuesli/BircherMuesliPlugin.h
@@ -2,22 +2,11 @@
 //  BircherMuesliPlugin.h
 //  BircherMuesli
 //
-//  Created by Jean-Pierre Mouilleseaux on 5/26/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Created by Jean-Pierre Mouilleseaux on 26 May 2011.
+//  Copyright 2011 Chorded Constructions. All rights reserved.
 //
 
 #import <Quartz/Quartz.h>
 
-@interface BircherMuesliPlugIn : QCPlugIn {
-@private
-    
-}
-
-/*
-Declare here the properties to be used as input and output ports for the plug-in e.g.
-@property double inputFoo;
-@property(assign) NSString* outputBar;
-You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
-*/
-
+@interface BircherMuesliPlugIn : QCPlugIn {}
 @end
