53164685f2f0048e57e017b73dec083642d5332b
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-05-26 19:57:30 -0700

    initial import of project skeleton

---
+++ BircherMuesli/BircherMuesliPlugin.h
@@ -0,0 +1,23 @@
+//
+//  BircherMuesliPlugin.h
+//  BircherMuesli
+//
+//  Created by Jean-Pierre Mouilleseaux on 5/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface BircherMuesliPlugIn : QCPlugIn {
+@private
+    
+}
+
+/*
+Declare here the properties to be used as input and output ports for the plug-in e.g.
+@property double inputFoo;
+@property(assign) NSString* outputBar;
+You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
+*/
+
+@end
