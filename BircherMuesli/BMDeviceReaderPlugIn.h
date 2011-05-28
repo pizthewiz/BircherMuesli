70c4254f6d136c88f00f2d0a0e4645002c86050f
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-05-27 20:40:19 -0700

    add skeleton for Serial Reader

---
+++ BircherMuesli/BMDeviceReaderPlugIn.h
@@ -0,0 +1,12 @@
+//
+//  BMDeviceReaderPlugIn.h
+//  BircherMuesli
+//
+//  Created by Jean-Pierre Mouilleseaux on 27 May 2011.
+//  Copyright 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface BMDeviceReaderPlugIn : QCPlugIn {}
+@end
---
+++ BircherMuesli/BMDeviceReaderPlugIn.m
@@ -0,0 +1,90 @@
+//
+//  BMDeviceReaderPlugIn.m
+//  BircherMuesli
+//
+//  Created by Jean-Pierre Mouilleseaux on 27 May 2011.
+//  Copyright 2011 Chorded Constructions. All rights reserved.
+//
+
+#import "BMDeviceReaderPlugIn.h"
+#import "BircherMuesli.h"
+
+@implementation BMDeviceReaderPlugIn
+
++ (NSDictionary*)attributes {
+	return [NSDictionary dictionaryWithObjectsAndKeys:
+        CCLocalizedString(@"DeviceReaderPlugInName", NULL), QCPlugInAttributeNameKey, 
+        CCLocalizedString(@"DeviceReaderPlugInDescription", NULL), QCPlugInAttributeDescriptionKey, 
+        // TODO - add QCPlugInAttributeCategoriesKey and QCPlugInAttributeExamplesKey
+        nil];
+}
+
++ (NSDictionary*)attributesForPropertyPortWithKey:(NSString*)key {
+	return nil;
+}
+
++ (QCPlugInExecutionMode)executionMode{
+	return kQCPlugInExecutionModeProvider;
+}
+
++ (QCPlugInTimeMode)timeMode {
+	return kQCPlugInTimeModeIdle;
+}
+
+#pragma mark -
+
+- (id)init {
+	self = [super init];
+	if (self) {
+	}
+	return self;
+}
+
+- (void)finalize {
+	[super finalize];
+}
+
+- (void)dealloc {
+	[super dealloc];
+}
+
+#pragma mark - EXECUTION
+
+- (BOOL)startExecution:(id <QCPlugInContext>)context {
+	/*
+     Called by Quartz Composer when rendering of the composition starts: perform any required setup for the plug-in.
+     Return NO in case of fatal failure (this will prevent rendering of the composition to start).
+     */
+
+	return YES;
+}
+
+- (void)enableExecution:(id <QCPlugInContext>)context {
+	/*
+     Called by Quartz Composer when the plug-in instance starts being used by Quartz Composer.
+     */
+}
+
+- (BOOL)execute:(id <QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary*)arguments {
+	/*
+     Called by Quartz Composer whenever the plug-in instance needs to execute.
+     Only read from the plug-in inputs and produce a result (by writing to the plug-in outputs or rendering to the destination OpenGL context) within that method and nowhere else.
+     Return NO in case of failure during the execution (this will prevent rendering of the current frame to complete).
+     */
+
+	return YES;
+}
+
+- (void)disableExecution:(id <QCPlugInContext>)context {
+	/*
+     Called by Quartz Composer when the plug-in instance stops being used by Quartz Composer.
+     */
+}
+
+- (void)stopExecution:(id <QCPlugInContext>)context {
+	/*
+     Called by Quartz Composer when rendering of the composition stops: perform any required cleanup for the plug-in.
+     */
+}
+
+@end
