8576d77a6e6e7555cf28897185d85cdf7194b644
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-05-31 00:05:12 -0700

    add bare device writer patch to the mix

---
+++ BircherMuesli/BMDeviceWriterPlugIn.h
@@ -0,0 +1,12 @@
+//
+//  BMDeviceWriterPlugIn.h
+//  BircherMuesli
+//
+//  Created by Jean-Pierre Mouilleseaux on 31 May 2011.
+//  Copyright 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface BMDeviceWriterPlugIn : QCPlugIn {}
+@end
