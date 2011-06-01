ebf81fc734cf70595951c5b1fcd29535f3a68982
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-05-31 22:51:29 -0700

    add a warning log macro

--- BircherMuesli/BircherMuesli.h
+++ BircherMuesli/BircherMuesli.h
@@ -9,10 +9,12 @@
 #ifdef DEBUG
     #define CCDebugLogSelector() NSLog(@"-[%@ %@]", /*NSStringFromClass([self class])*/self, NSStringFromSelector(_cmd))
     #define CCDebugLog(a...) NSLog(a)
+    #define CCWarningLog(a...) NSLog(a)
     #define CCErrorLog(a...) NSLog(a)
 #else
     #define CCDebugLogSelector()
     #define CCDebugLog(a...)
+    #define CCWarningLog(a...) NSLog(a)
     #define CCErrorLog(a...) NSLog(a)
 #endif
 
