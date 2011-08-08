6fb6c7102eafd59063b363e1ea0891dfd14fe861
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-08-08 13:00:36 -0700

    add NSString category

---
+++ BircherMuesli/NSString+CCExtensions.h
@@ -0,0 +1,12 @@
+//
+//  NSString+CCExtensions.h
+//  BircherMuesli
+//
+//  Created by Jean-Pierre Mouilleseaux on 8 Aug 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString(CCExtensions)
+@end
---
+++ BircherMuesli/NSString+CCExtensions.m
@@ -0,0 +1,12 @@
+//
+//  NSString+CCExtensions.m
+//  BircherMuesli
+//
+//  Created by Jean-Pierre Mouilleseaux on 8 Aug 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import "NSString+CCExtensions.h"
+
+@implementation NSString(CCExtensions)
+@end
